db2b0ccfab15fb33147c7f5d94bbae653eecc996
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-05-18 19:31:01 +0600

    add the recharts

--- src/app/(main)/stats/page.jsx
+++ src/app/(main)/stats/page.jsx
@@ -1,12 +1,46 @@
+"use client";
+
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+
 const StatsPage = () => {
+  const data = [
+    { name: "Call", value: 400, fill: "#7E35E1" },
+    { name: "Text", value: 300, fill: "#244D3F" },
+    { name: "Video", value: 300, fill: "#37A163" },
+  ];
   return (
     <div className="min-h-screen bg-base-300">
       <div className="container mx-auto py-20 space-y-4">
         <h1 className="text-5xl font-bold">Friendship Analytics</h1>
         <div className="card container mx-auto lg:card-side bg-base-100 shadow-sm">
           <div className="card-body">
-            <h2 className="font-bold text-green-900">By Interaction Type</h2>
-
+            <h2 className="font-bold text-green-900 text-xlj">By Interaction Type</h2>
+            <div className="flex justify-center items-center">
+              <PieChart
+                style={{
+                  width: "100%",
+                  maxWidth: "500px",
+                  maxHeight: "80vh",
+                  aspectRatio: 1,
+                }}
+                responsive
+              >
+                <Pie
+                  data={data}
+                  innerRadius="80%"
+                  outerRadius="100%"
+                  // Corner radius is the rounded edge of each pie slice
+                  cornerRadius="80%"
+                  fill="#8884d8"
+                  // padding angle is the gap between each pie slice
+                  paddingAngle={5}
+                  dataKey="value"
+                  isAnimationActive={true}
+                />
+                <Legend></Legend>
+                <Tooltip></Tooltip>
+              </PieChart>
+            </div>
           </div>
         </div>
       </div>
